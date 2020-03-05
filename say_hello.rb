2e6267b523dfe325de628d3e078cc9b386e3398b
Pablo Menz <p.menz.r@gmail.com>
2020-03-05 02:18:16 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
-# Build your say_hello method here
+def say_hello(name='Ruby Programmer!')
+  puts "Hello #{name}"
+  
+end
+  # Build your say_hello method here
+  
+  
+  name= "kent Beck!"
+  say_hello(name)
+  say_hello()
